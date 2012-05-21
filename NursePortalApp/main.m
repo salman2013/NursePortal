21b4e110a99e4ad3cb5188617b4c175dac30a119
Maverick <user@Mavericks-MBP.local>
2012-05-21 12:54:57 +0500

    Initial Commit

---
+++ NursePortalApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NursePortalApp
+//
+//  Created by user on 21/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IYAppDelegate class]));
+    }
+}
